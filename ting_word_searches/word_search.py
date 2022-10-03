fbd10c951975e9aeb0fba9d37b9b7806330126dd
Rodrigo Lima <limarodrigoo@outlook.com>
2022-10-03 15:28:32 -0300

    feat: implement exists word method

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for text in instance._data:
+        occurrence = list()
+        for index, line in enumerate(text["linhas_do_arquivo"]):
+
+            if word.lower() in line.lower():
+                occurrence.append({"linha": index + 1})
+        if len(occurrence) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": text["nome_do_arquivo"],
+                    "ocorrencias": occurrence,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):

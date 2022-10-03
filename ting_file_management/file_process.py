5238f089a6a6c55fee12f53688db0f8d7ea3a09c
Rodrigo Lima <limarodrigoo@outlook.com>
2022-10-03 15:26:15 -0300

    feat: implement process file method

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in instance._data:
+        if path_file == i["nome_do_arquivo"]:
+            return None
+
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
@@ -8,3 +24,4 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
